7438afd7d3bc8ea6328fcc56af9dcb9b97cb6ee4
hahaqxy <2847780939@qq.com>
2020-07-06 16:04:06 +0800

    second commit

--- packages/components/DataCard/index.vue
+++ packages/components/DataCard/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="data-card">
+    <div :class="bemCss()">
         <el-row :span="24">
             <el-col
                     :xs="24"
@@ -8,16 +8,16 @@
                     v-for="(item, index) in data"
                     :key="index"
             >
-                <div class="item">
+                <div :class="bemCss('item')">
                     <a
                             :href="item.href ? item.href : 'javascript:void(0);'"
-                            @click="item.click ? item.click(item) : ''"
                             :target="item.target"
+                            @click="itemClick ? itemClick(item, index) : ''"
                     >
-                        <img class="item-img" :src="item.src" />
-                        <div class="item-text" :style="{ backgroundColor: bgText }">
-                            <h3 :style="{ color: colorText }">{{ item.name }}</h3>
-                            <p :style="{ color: colorText }">{{ item.text }}</p>
+                        <img :class="bemCss('itemImg')" :src="item.src" />
+                        <div :class="bemCss('itemText')" :style="{ backgroundColor: bgText, color: colorText }">
+                            <h3>{{ item.name }}</h3>
+                            <p>{{ item.text }}</p>
                         </div>
                     </a>
                 </div>
@@ -28,11 +28,12 @@
 
 <script>
     import create from "../../core/create";
-
     export default create({
         name: "data-card",
         data: function() {
-            return {};
+            return {
+                itemClick: this.option.itemClick
+            };
         },
         props: {
             option: {
@@ -53,56 +54,46 @@
             colorText: function() {
                 return this.option.colorText || "#fff";
             }
-        },
-        created() {},
-        mounted() {},
-        watch: {},
-        methods: {}
+        }
     });
 </script>
 
 <style scoped lang="scss">
-    .data-card {
-        $height: 340px;
-    .item {
-        position: relative;
-        margin: 0 auto;
-        margin-bottom: 50px;
-        width: 230px;
-        height: $height;
-        overflow: hidden;
-        border-radius: 5px;
-        border-color: #fff;
-        border-width: 1px;
-        border-style: solid;
-    &:hover .item-text {
-         top: 0;
-     }
-    }
-    .item-img {
-        width: 100%;
-        border-radius: 5px;
-        border-bottom-left-radius: 0;
-        border-bottom-right-radius: 0;
-    }
-    .item-text {
-        position: absolute;
-        top: 150px;
-        padding: 20px 15px;
-        width: 100%;
-        height: $height;
-        overflow: auto;
-        box-sizing: border-box;
-        border-radius: 5px;
-        border-top-left-radius: 0;
-        border-top-right-radius: 0;
-        opacity: 0.9;
-        transition: top 0.4s;
-    &>p {
-         font-size: 16px;
-         line-height: 25px;
-         text-indent: 2em;
-     }
-    }
+    $height: 340px;
+    .hxvue-data-card {
+        &__item {
+            position: relative;
+            margin: 0 auto;
+            margin-bottom: 50px;
+            width: 230px;
+            height: $height;
+            overflow: hidden;
+            border: 1px solid #fff;
+            border-radius: 5px;
+            &:hover .hxvue-data-card__itemText {
+             top: 0;
+         }
+        }
+        &__itemImg {
+            width: 100%;
+            border-radius: 5px 5px 0 0;
+        }
+        &__itemText {
+            position: absolute;
+            top: 150px;
+            padding: 20px 15px;
+            width: 100%;
+            height: $height;
+            overflow: auto;
+            box-sizing: border-box;
+            border-radius: 0 0 5px 5px;
+            opacity: 0.9;
+            transition: top 0.4s;
+            p {
+             font-size: 16px;
+             line-height: 25px;
+             text-indent: 2em;
+         }
+        }
     }
 </style>
--- packages/components/FilterDrawer/index.vue
+++ packages/components/FilterDrawer/index.vue
@@ -1,23 +1,20 @@
 <template>
-    <div class="filter-drawer">
-        <div class="filter_button"
-             :style="{
-             width: buttonWidth
-             }"
+    <div :class="bemCss()">
+        <div :class="bemCss('filter_button')"
+             :style="{ width: visible ? '265px' : buttonWidth }"
               @click="handleClick"
         >
             筛选
         </div>
         <transition name="drawer-fade">
             <div
-                    class="wrapper"
+                    :class="bemCss('wrapper')"
                     v-if="visible"
                     :style="{
           width: drawerWidth,
           height: drawerHeight,
           backgroundColor: backgroundColor
         }"
-                    ref="drawer"
             >
                 <slot :name="slotName"></slot>
             </div>
@@ -34,24 +31,40 @@
             return {
                 visible: false,
                 slotName: "sort",
-                drawerWidth: "265px",
-                drawerHeight: "800px",
-                backgroundColor: "#353838",
-                buttonWidth: "180px"
-
             };
+        },
+        props: {
+            option: {
+                type: Object,
+                default: () => {
+                    return {};
+                }
+            }
+        },
+        computed: {
+            drawerWidth: function() {
+                return this.option.drawerWidth || "20%";
+            },
+            drawerHeight: function() {
+                return this.option.drawerHeight || "15%";
+            },
+            backgroundColor: function() {
+                return this.option.backgroundColor || "#000";
+            },
+            buttonWidth: function() {
+                return this.option.buttonWidth || "15%";
+            }
         },
         methods: {
             handleClick: function () {
                 this.visible = ! this.visible;
-                this.buttonWidth = this.buttonWidth === "180px" ? "265px" : "180px";
             }
         }
     })
 </script>
 
 <style scoped lang="scss">
-    $time: 0.5s;
+    $time: 0.2s;
     $fontColor: #fff;
     @mixin positionInfo(
         $top: 0%,
@@ -78,26 +91,23 @@
         }
     }
 
-    .filter-drawer {
+    .hxvue-filter-drawer {
         color: $fontColor;
 
-        .filter_button {
-            position: fixed;
-            top: 0;
-            left: 0;
-            height: 70px;
+        &__filter_button {
+            @include positionInfo(0, 0, 100%, 70px);
             background-color: #353838;
             font-size: 20px;
-            padding: 21px 40px 0px 60px;
             border-bottom: 1px solid #0c0d0d;
-            text-align: left;
-            white-space: nowrap;
-            box-sizing: border-box;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            text-align: center;
             transition: width $time ease-in-out;
         }
 
-        .wrapper {
-            @include positionInfo(70px, 0%, 0%, 100%);
+        &__wrapper {
+            @include positionInfo(71px, 0%, 0%, 100%);
             padding: 30px 20px 20px 40px;
             box-sizing: border-box;
         }
